c2ea212ad0f6185688a18a17217d6750a182c129
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-06 13:40:47 +0300

    переделка линтером

--- src/microblogging/microblogging/settings.py
+++ src/microblogging/microblogging/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG_MODE"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
