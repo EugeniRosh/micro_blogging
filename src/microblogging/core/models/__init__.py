c3bde5fbae6e38a2d4958790a8c1c5f3fe4f0123
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-06 20:03:56 +0300

    init file models

---
+++ src/microblogging/core/models/__init__.py
@@ -0,0 +1,19 @@
+from .followers import Followers
+from .profile import Profiles
+from .tags import Tags
+from .twits import Twits
+from .twits_profiles_likes import TwitsProfilesLikes
+from .twits_profiles_notifications import TwitsProfilesNotifications
+from .twits_profiles_reposts import TwitsProfilesReposts
+from .twits_tags import TwitsTags
+
+__all__ = [
+    "Followers",
+    "Profiles",
+    "Tags",
+    "TwitsProfilesLikes",
+    "TwitsProfilesNotifications",
+    "TwitsProfilesReposts",
+    "TwitsTags",
+    "Twits",
+]
