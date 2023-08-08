59948a3abd4b040e7a84d9eb138005520bf75c87
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-08 19:59:37 +0300

    add new path registration

--- src/microblogging/core/presentation/urls.py
+++ src/microblogging/core/presentation/urls.py
@@ -1,6 +1,7 @@
-from core.presentation.views import index_controller
+from core.presentation.views import index_controller, registrations_controller
 from django.urls import path
 
 urlpatterns = [
     path("", index_controller, name="index"),
+    path("signup/", registrations_controller, name="registration"),
 ]
