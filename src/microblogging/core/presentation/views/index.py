7bfa44757e1ba8e8d675e034e132613d70571d1f
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-12 15:42:17 +0300

    add login check

--- src/microblogging/core/presentation/views/index.py
+++ src/microblogging/core/presentation/views/index.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
@@ -9,6 +10,7 @@ if TYPE_CHECKING:
     from django.http import HttpRequest, HttpResponse
 
 
+@login_required
 @require_http_methods(["GET"])
 def index_controller(request: HttpRequest) -> HttpResponse:
     context = {"title": "MICROBLOG"}
