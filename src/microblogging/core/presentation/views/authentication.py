6ffab6111e6ec0564469da50195e0bea3968549b
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-12 15:44:14 +0300

    add authentication_controller,  logout_controller

---
+++ src/microblogging/core/presentation/views/authentication.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from core.bussiness_logic.dto import AuthenticationDTO
+from core.bussiness_logic.exeptions import GetValueError
+from core.bussiness_logic.servises import (
+    authentication_user,
+    convert_data_from_form_in_dacite,
+    logout_user,
+)
+from core.presentation.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, HttpResponse
+
+
+@require_http_methods(["GET", "POST"])
+def authentication_controller(request: HttpRequest) -> HttpResponse:
+    form = AuthenticationForm()
+
+    context = {"title": "Authentication"}
+
+    if request.POST:
+        form_authentication = AuthenticationForm(request.POST)
+        if form_authentication.is_valid():
+            data = convert_data_from_form_in_dacite(
+                dto=AuthenticationDTO, data=form_authentication.cleaned_data
+            )
+            try:
+                authentication_user(request=request, data=data)
+                return redirect(to="index")
+            except GetValueError:
+                form = form_authentication
+                context.update({"message": "Incorrect email or password."})
+        else:
+            form = form_authentication
+
+    context.update({"form": form})
+    return render(request=request, template_name="authentication.html", context=context)
+
+
+@require_http_methods(["GET"])
+def logout_controller(request: HttpRequest) -> HttpResponse:
+    logout_user(request=request)
+    return redirect(to="index")
