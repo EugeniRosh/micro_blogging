81bbad0adc0f4dc41916b67f84cf18f67c9a2d66
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-12 15:44:56 +0300

    add authentication and  logout path

--- src/microblogging/microblogging/urls.py
+++ src/microblogging/microblogging/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core.presentation.views import authentication_controller, logout_controller
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.presentation.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("core.presentation.urls")),
+    path("authentication/", authentication_controller, name="authentication"),
+    path("logout/", logout_controller, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
