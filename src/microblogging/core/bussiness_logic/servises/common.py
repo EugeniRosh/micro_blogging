36677ea275bff66398593937d59f7a9f373d92ad
EugeniRosh <122040519+EugeniRosh@users.noreply.github.com>
2023-08-13 11:36:58 +0300

    Fix/queries registration (#7)
    * optimization queries
    
    * add print_queries
    
    ---------
    
    Co-authored-by: YauheniRoshchanka <roshchenkoevgen@gmail.com>

--- src/microblogging/core/bussiness_logic/servises/common.py
+++ src/microblogging/core/bussiness_logic/servises/common.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Any
 
 from dacite import from_dict
+from django.db import connection
 
 
 def convert_data_from_form_in_dacite(dto: Any, data: dict[str, Any]) -> Any:
@@ -12,3 +13,15 @@ def convert_data_from_form_in_dacite(dto: Any, data: dict[str, Any]) -> Any:
 def parsing_create_unique_error_message(err: Exception) -> str:
     err_split = str(err).split(".")
     return err_split[-1]
+
+
+def print_queries() -> None:
+    print("============")
+    queries_count = 0
+    for i in connection.queries:
+        print(i)
+        queries_count += 1
+
+    print(queries_count)
+    print("============")
+    return
