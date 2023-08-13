56aa065c275251fbdc5c156441bc3a62da008bbe
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-13 18:45:09 +0300

    add profile path

--- src/microblogging/core/presentation/urls.py
+++ src/microblogging/core/presentation/urls.py
@@ -1,7 +1,17 @@
-from core.presentation.views import index_controller, registrations_controller
-from django.urls import path
+from core.presentation.views import (
+    index_controller,
+    profile_controller,
+    registrations_controller,
+)
+from django.urls import include, path
+
+profile_patterns = [
+    path("", profile_controller, name="profile"),
+]
+
 
 urlpatterns = [
     path("", index_controller, name="index"),
     path("signup/", registrations_controller, name="registration"),
+    path("profile/<username>/", include(profile_patterns)),
 ]
