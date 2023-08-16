4ea8d078acdf073b4069105e6fd44f19dc92d786
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-16 17:18:40 +0300

    add profile_following, profile_followers

--- src/microblogging/core/presentation/views/profile.py
+++ src/microblogging/core/presentation/views/profile.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from core.bussiness_logic.exeptions import GetValueError
-from core.bussiness_logic.servises import get_user_profile
+from core.bussiness_logic.servises import get_followers, get_following, get_user_profile
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
@@ -16,9 +16,30 @@ if TYPE_CHECKING:
 @require_http_methods(["GET"])
 def profile_controller(request: HttpRequest, username: str) -> HttpResponse:
     try:
-        profile = get_user_profile(username=username)
+        profile, followers_num = get_user_profile(username=username)
     except GetValueError:
         return redirect(to="index")
 
-    context = {"title": "Profile", "profile": profile}
+    context = {"title": "Profile", "profile": profile, "followers_num": followers_num}
     return render(request=request, template_name="profile.html", context=context)
+
+
+@login_required()
+@require_http_methods(["GET"])
+def profile_following_controller(request: HttpRequest, username: str) -> HttpResponse:
+    following = get_following(username=username)
+    context = {"title": "Following", "following": following}
+    print(following)
+    return render(
+        request=request, template_name="profile_followings.html", context=context
+    )
+
+
+@login_required()
+@require_http_methods(["GET"])
+def profile_followers_controller(request: HttpRequest, username: str) -> HttpResponse:
+    followers = get_followers(username=username)
+    context = {"title": "Followers", "followers": followers}
+    return render(
+        request=request, template_name="profile_followers.html", context=context
+    )
