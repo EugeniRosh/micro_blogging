8d5357719882593672edf188ea2d88916994a97b
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-16 17:16:54 +0300

    add ProfileDTO, CountFollowersProfileDTO

--- src/microblogging/core/bussiness_logic/dto/__init__.py
+++ src/microblogging/core/bussiness_logic/dto/__init__.py
@@ -1,7 +1,10 @@
 from .authentication import AuthenticationDTO
+from .profile import CountFollowersProfileDTO, ProfileDTO
 from .registration import RegistrationDTO
 
 __all__ = [
     "RegistrationDTO",
     "AuthenticationDTO",
+    "ProfileDTO",
+    "CountFollowersProfileDTO",
 ]
