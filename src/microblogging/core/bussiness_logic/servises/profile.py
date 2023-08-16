926ff98023577a8ebd3eb803bc9dfad41e9916f4
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-16 17:17:14 +0300

    update get_user_profile

--- src/microblogging/core/bussiness_logic/servises/profile.py
+++ src/microblogging/core/bussiness_logic/servises/profile.py
@@ -2,19 +2,21 @@ from __future__ import annotations
 
 import logging
 
+from core.bussiness_logic.dto import CountFollowersProfileDTO, ProfileDTO
 from core.bussiness_logic.exeptions import GetValueError
 from core.models import Profiles
 from django.db.models import Count
 
+from .common import convert_data_from_form_in_dacite
+
 logger = logging.getLogger(__name__)
 
 
-def get_user_profile(username: str) -> list[Profiles]:
+def get_user_profile(username: str) -> tuple[ProfileDTO, CountFollowersProfileDTO]:
     profile = (
-        Profiles.objects.prefetch_related("notification", "followers")
-        .annotate(
-            count_followers=Count("followers__follower_profile"),
-            count_following=Count("followers__follower"),
+        Profiles.objects.annotate(
+            count_followers=Count("user_following", distinct=True),
+            count_following=Count("user_follower", distinct=True),
         )
         .values(
             "username",
@@ -27,10 +29,15 @@ def get_user_profile(username: str) -> list[Profiles]:
             "count_followers",
             "count_following",
         )
-        .filter(username=username)
+        .get(username=username)
     )
+
     if not profile:
         raise GetValueError
+    profile_dto = convert_data_from_form_in_dacite(dto=ProfileDTO, data=profile)
+    counter_followers = convert_data_from_form_in_dacite(
+        dto=CountFollowersProfileDTO, data=profile
+    )
 
     logger.info(f"Get info user: {username}")
-    return list(profile)
+    return profile_dto, counter_followers
