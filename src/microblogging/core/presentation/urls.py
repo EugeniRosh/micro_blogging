14f52fa7f0cbb5a57e67e131e44deebd3c70feb1
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-16 17:19:32 +0300

    add followers, following path

--- src/microblogging/core/presentation/urls.py
+++ src/microblogging/core/presentation/urls.py
@@ -1,12 +1,16 @@
 from core.presentation.views import (
     index_controller,
     profile_controller,
+    profile_followers_controller,
+    profile_following_controller,
     registrations_controller,
 )
 from django.urls import include, path
 
 profile_patterns = [
     path("", profile_controller, name="profile"),
+    path("followers/", profile_followers_controller, name="followers"),
+    path("following/", profile_following_controller, name="following"),
 ]
 
 
