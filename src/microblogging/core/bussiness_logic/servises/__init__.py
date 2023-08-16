9081ee8872dc01bbc3daadce7ea9a19e7fdedaa0
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-16 17:17:41 +0300

    add get_following, get_followers

--- src/microblogging/core/bussiness_logic/servises/__init__.py
+++ src/microblogging/core/bussiness_logic/servises/__init__.py
@@ -3,6 +3,7 @@ from .common import (
     convert_data_from_form_in_dacite,
     parsing_create_unique_error_message,
 )
+from .followers import get_followers, get_following
 from .profile import get_user_profile
 from .registration import regisration_user, registration_confirmations
 
@@ -15,4 +16,6 @@ __all__ = [
     "authentication_user",
     "logout_user",
     "get_user_profile",
+    "get_following",
+    "get_followers",
 ]
