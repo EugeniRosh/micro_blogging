3154914e1594088a2b5f9df3ae9751007aebd8a8
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-18 12:30:18 +0300

    add rename_in_uuid, change_file_size, change_photo

--- src/microblogging/core/bussiness_logic/servises/profile.py
+++ src/microblogging/core/bussiness_logic/servises/profile.py
@@ -1,43 +1,44 @@
 from __future__ import annotations
 
 import logging
+from typing import TYPE_CHECKING
 
-from core.bussiness_logic.dto import CountFollowersProfileDTO, ProfileDTO
-from core.bussiness_logic.exeptions import GetValueError
+from core.bussiness_logic.exeptions import CreateUniqueError, GetValueError
 from core.models import Profiles
 from django.db.models import Count
+from django.db.utils import IntegrityError
 
-from .common import convert_data_from_form_in_dacite
+from .common import change_photo
+
+if TYPE_CHECKING:
+    from django.db.models.query import QuerySet
+    from django.http.request import QueryDict
+    from django.utils.datastructures import MultiValueDict
 
 logger = logging.getLogger(__name__)
 
 
-def get_user_profile(username: str) -> tuple[ProfileDTO, CountFollowersProfileDTO]:
-    profile = (
-        Profiles.objects.annotate(
+def get_user_profile(username: str) -> QuerySet:
+    try:
+        profile = Profiles.objects.annotate(
             count_followers=Count("user_following", distinct=True),
             count_following=Count("user_follower", distinct=True),
-        )
-        .values(
-            "username",
-            "photo",
-            "first_name",
-            "last_name",
-            "date_joined",
-            "country",
-            "description",
-            "count_followers",
-            "count_following",
-        )
-        .get(username=username)
-    )
-
-    if not profile:
+        ).get(username=username)
+    except Profiles.DoesNotExist:
         raise GetValueError
-    profile_dto = convert_data_from_form_in_dacite(dto=ProfileDTO, data=profile)
-    counter_followers = convert_data_from_form_in_dacite(
-        dto=CountFollowersProfileDTO, data=profile
-    )
 
     logger.info(f"Get info user: {username}")
-    return profile_dto, counter_followers
+    return profile
+
+
+def edit_profile(username: str, data: QueryDict, files: MultiValueDict) -> None:
+    if "photo" in files:
+        data = change_photo(field="photo", data=files)
+
+    try:
+        Profiles.objects.update_or_create(username=username, defaults=data)
+        logger.info(f"Profile edit. username: {username}. {data}")
+    except IntegrityError as err:
+        raise CreateUniqueError(err)
+
+    return None
