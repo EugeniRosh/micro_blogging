34afa836d1ca17a2970b5742ef38197ae6bc55f5
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-23 21:42:49 +0300

    add ValidateFileSize, ValidateFileExtension

--- src/microblogging/core/presentation/validators.py
+++ src/microblogging/core/presentation/validators.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import os
 from datetime import datetime
 
 from django.core.exceptions import ValidationError
+from django.core.files import File
 
 
 class ValidationAge:
@@ -26,3 +28,27 @@ class ValidationAge:
                 )
 
         return None
+
+
+class ValidateFileExtension:
+    def __init__(self, available_extensions: list[str]) -> None:
+        self._available_extensions = available_extensions
+
+    def __call__(self, value: File) -> None:
+        _, file_extension = os.path.splitext(value.name)
+
+        if not file_extension:
+            raise ValidationError(message="File must have an extension")
+
+        if file_extension[1:] not in self._available_extensions:
+            raise ValidationError(message=f"Accept only {self._available_extensions}")
+
+
+class ValidateFileSize:
+    def __init__(self, max_size: int) -> None:
+        self._max_size = max_size
+
+    def __call__(self, value: File) -> None:
+        if value.size > self._max_size:
+            max_size_in_mb = int(self._max_size / 1_000_000)
+            raise ValidationError(message=f"Max file size is {max_size_in_mb} MB")
