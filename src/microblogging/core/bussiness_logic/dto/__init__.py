292153cdadc3d5da284729f148e1538e95302c29
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-24 18:01:08 +0300

    add PaginationPageDTO

--- src/microblogging/core/bussiness_logic/dto/__init__.py
+++ src/microblogging/core/bussiness_logic/dto/__init__.py
@@ -1,4 +1,5 @@
 from .authentication import AuthenticationDTO
+from .pagination import PaginationPageDTO
 from .profile import ProfileDTO
 from .registration import RegistrationDTO
 
@@ -6,4 +7,5 @@ __all__ = [
     "RegistrationDTO",
     "AuthenticationDTO",
     "ProfileDTO",
+    "PaginationPageDTO",
 ]
