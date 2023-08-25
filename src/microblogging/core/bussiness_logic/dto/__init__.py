fca20175ec685ae80627040285d679fddc21be01
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-25 14:05:11 +0300

    add TwitsDTO

--- src/microblogging/core/bussiness_logic/dto/__init__.py
+++ src/microblogging/core/bussiness_logic/dto/__init__.py
@@ -2,10 +2,12 @@ from .authentication import AuthenticationDTO
 from .pagination import PaginationPageDTO
 from .profile import ProfileDTO
 from .registration import RegistrationDTO
+from .twits import TwitsDTO
 
 __all__ = [
     "RegistrationDTO",
     "AuthenticationDTO",
     "ProfileDTO",
     "PaginationPageDTO",
+    "TwitsDTO",
 ]
