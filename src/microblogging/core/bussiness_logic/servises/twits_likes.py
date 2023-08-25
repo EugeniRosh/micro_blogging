5d7cd277d19e7dd6fa7e578b615bd96ecfed7678
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-25 18:16:57 +0300

    rename

--- src/microblogging/core/bussiness_logic/servises/twits_likes.py
+++ src/microblogging/core/bussiness_logic/servises/twits_likes.py
@@ -5,18 +5,18 @@ from typing import TYPE_CHECKING
 from .twits import get_tweet_by_id
 
 if TYPE_CHECKING:
-    from core.models import Profiles
+    from core.models import Profiles, Twits
 
 
 def add_like_twits(twit_id: int, profile: Profiles) -> None:
-    twit = get_tweet_by_id(twit_id=twit_id)
+    twit: Twits = get_tweet_by_id(twit_id=twit_id)
 
     twit.like.add(profile)
     return None
 
 
 def delete_like_twits(twit_id: int, profile: Profiles) -> None:
-    twit = get_tweet_by_id(twit_id=twit_id)
+    twit: Twits = get_tweet_by_id(twit_id=twit_id)
 
     twit.like.remove(profile)
     return None
