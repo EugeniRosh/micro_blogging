628125d25b4e401191f8be8198272dbb0415126e
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-29 17:36:04 +0300

    add Command

---
+++ src/microblogging/core/management/commands/populate_db.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from core.management.data_for_populate_db import (
+    PopulateFollowersRecord,
+    PopulateLikesRecord,
+    PopulateNotificationsRecord,
+    PopulateProfileRecord,
+    PopulateRepostsRecord,
+    PopulateTagsRecord,
+    PopulateTwitsRecord,
+)
+from django.core.management.base import BaseCommand
+
+if TYPE_CHECKING:
+    from django.core.management.base import CommandParser
+
+
+class Command(BaseCommand):
+    def __init__(self) -> None:
+        self.tags = PopulateTagsRecord()
+        self.profiles = PopulateProfileRecord()
+        self.followers = PopulateFollowersRecord()
+        self.twits = PopulateTwitsRecord()
+        self.likes = PopulateLikesRecord()
+        self.reposts = PopulateRepostsRecord()
+        self.notifications = PopulateNotificationsRecord()
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("value", nargs="+", type=int)
+
+    def handle(self, *args: tuple[Any, ...], **options: dict[Any, Any]) -> None:
+        print('+++', type(args), type(options), "++++")
+        if options["value"]:
+            value = options["value"][0]
+        else:
+            value = 100
+
+        tags = self.tags(value_count=value)
+
+        profiles = self.profiles(value_count=value)
+
+        twits = self.twits(value_count=value, profiles=profiles, tags=tags)
+
+        likes = self.likes(profiles=profiles, twits=twits, max_value=value)
+
+        self.reposts(profiles=profiles, twits=twits, max_value=value)
+
+        self.notifications(notifications=likes)
+
+        self.followers(profiles=profiles, max_value=value)
+
+        return None
