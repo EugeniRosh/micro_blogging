47864e9c44ee00b227a2380a3a17fff1880ba6cd
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-29 17:27:58 +0300

    init file

---
+++ src/microblogging/core/management/data_for_populate_db/provider/__init__.py
@@ -0,0 +1,11 @@
+from .country_provider import CountryProvider
+from .name_provider import NameProvider
+from .surname_provider import SurnameProvider
+from .word_provider import WordProvider
+
+__all__ = [
+    "WordProvider",
+    "NameProvider",
+    "SurnameProvider",
+    "CountryProvider",
+]
