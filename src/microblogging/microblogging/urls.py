df1bb350c28f21015d3e71a1669b949c511921f8
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-09-03 22:28:36 +0300

    delete confirm_signup, authentication, logout

--- src/microblogging/microblogging/urls.py
+++ src/microblogging/microblogging/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from core.presentation.views import authentication_controller, logout_controller
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,6 +22,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.presentation.urls")),
-    path("authentication/", authentication_controller, name="authentication"),
-    path("logout/", logout_controller, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
