bb70f6b6c79fdcdc98a55a0391e43e12058665de
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-09-05 17:42:28 +0300

    udate parsing error func

--- src/microblogging/core/bussiness_logic/servises/__init__.py
+++ src/microblogging/core/bussiness_logic/servises/__init__.py
@@ -1,7 +1,7 @@
 from .authentication import authentication_user, logout_user
 from .common import (
     convert_data_from_form_in_dacite,
-    parsing_create_unique_error_message,
+    parsing_the_unique_creation_error_in_postgres,
 )
 from .followers import get_followers, get_following
 from .profile import (
@@ -32,8 +32,7 @@ __all__ = [
     "regisration_user",
     "registration_confirmations",
     "convert_data_from_form_in_dacite",
-    "parsing_create_unique_error_massage",
-    "parsing_create_unique_error_message",
+    "parsing_the_unique_creation_error_in_postgres",
     "authentication_user",
     "logout_user",
     "get_user_profile",
