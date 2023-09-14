8f70ea96aa2c5c62a8e13c78a7f21badb726b7b7
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-09-14 15:51:56 +0300

    add logic get twit

--- src/microblogging/core/bussiness_logic/servises/twits_likes.py
+++ src/microblogging/core/bussiness_logic/servises/twits_likes.py
@@ -2,15 +2,27 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from .twits import get_twit_by_id
+
 if TYPE_CHECKING:
     from core.models import Profiles, Twits
 
 
-def add_like_twits(twit: Twits, profile: Profiles) -> None:
+def add_like_twits(twit_id: int, profile: Profiles) -> None:
+    twit: Twits = get_twit_by_id(twit_id=twit_id)
+
+    if twit.profile == profile:
+        return None
+
     twit.like.add(profile)
     return None
 
 
-def delete_like_twits(twit: Twits, profile: Profiles) -> None:
+def delete_like_twits(twit_id: int, profile: Profiles) -> None:
+    twit: Twits = get_twit_by_id(twit_id=twit_id)
+
+    if twit.profile == profile:
+        return None
+
     twit.like.remove(profile)
     return None
