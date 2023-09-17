be7344d3261aba3393bec5fd8b102661d179ca4e
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-09-17 17:31:50 +0300

    add index_controller

--- src/microblogging/core/presentation/views/index.py
+++ src/microblogging/core/presentation/views/index.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from core.business_logic.exceptions import PaginationError
+from core.business_logic.services import get_twits_to_index_page
+from core.presentation.paginator import CustomPaginator
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
@@ -13,5 +17,26 @@ if TYPE_CHECKING:
 @login_required
 @require_http_methods(["GET"])
 def index_controller(request: HttpRequest) -> HttpResponse:
-    context = {"title": "MICROBLOG"}
+    try:
+        sort_string = request.GET["sort-string"]
+    except KeyError:
+        sort_string = "created_at"
+
+    twits = get_twits_to_index_page(profile=request.user, sort_string=sort_string)
+    try:
+        page_num = request.GET["page"]
+    except KeyError:
+        page_num = 1
+    paginator = CustomPaginator(max_value=20)
+    try:
+        twits_paginator = paginator.paginate(data=twits, page_num=page_num)
+    except PaginationError:
+        return HttpResponseBadRequest(
+            content="Page with provided number doesn't exist."
+        )
+    context = {
+        "title": "MICROBLOG",
+        "twits": twits_paginator,
+        "sort_string": sort_string,
+    }
     return render(request=request, template_name="index.html", context=context)
