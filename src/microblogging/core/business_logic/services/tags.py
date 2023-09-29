2bc1d468b8f4406dcc043d324eec8e8006473424
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-09-29 18:37:49 +0300

    added trending_in_your_country

--- src/microblogging/core/business_logic/services/tags.py
+++ src/microblogging/core/business_logic/services/tags.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from datetime import datetime, timedelta
 from typing import TYPE_CHECKING
 
 from core.models import Tags
+from django.db.models import Count
 
 if TYPE_CHECKING:
     from django.db.models.query import QuerySet
@@ -23,3 +25,19 @@ def get_tags(tags: str) -> list[Tags]:
 def get_a_tag_to_search_for_tweets(tag: str) -> QuerySet:
     search_tag = Tags.objects.filter(tag=tag)
     return search_tag
+
+
+def trending_in_your_country(country: str) -> QuerySet:
+    date = datetime.today()
+    date_to_search = date - timedelta(days=1)
+
+    tags = (
+        Tags.objects.filter(
+            twit_tag__twit__profile__country=country,
+            twit_tag__twit__created_at__date=date_to_search.date(),
+        )
+        .annotate(count_tag=Count("tag"))
+        .order_by("-count_tag")[:10]
+    )
+
+    return tags
