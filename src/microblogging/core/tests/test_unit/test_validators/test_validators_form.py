c268a6f09894def5b2baf6886093c012d355b0e2
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-10-16 18:23:08 +0300

    added validate file

--- src/microblogging/core/tests/test_unit/test_validators/test_validators_form.py
+++ src/microblogging/core/tests/test_unit/test_validators/test_validators_form.py
@@ -2,7 +2,13 @@
 from datetime import datetime
 
 import pytest
-from core.presentation.validators import ValdateMaxValue, ValidationAge
+from core.presentation.validators import (
+    ValdateMaxValue,
+    ValidateFileExtension,
+    ValidateFileSize,
+    ValidationAge,
+)
+from core.tests.mocks import FileMock
 from django.core.exceptions import ValidationError
 
 
@@ -55,3 +61,36 @@ class TestValdateMaxValue:
         value_str = "\r\n".join(value)
         with pytest.raises(ValidationError):
             self.validation(value=value_str)
+
+
+class TestValidateFileExtension:
+    validation = ValidateFileExtension(available_extensions=["pdf"])
+
+    def test_validation_file_extension_successfully(self) -> None:
+        file_mock = FileMock(name="test.pdf")
+        result = self.validation(value=file_mock)
+        assert result is None
+
+    def test_validation_missing_file_extension(self) -> None:
+        file_mock = FileMock(name="test")
+        with pytest.raises(ValidationError):
+            self.validation(value=file_mock)
+
+    def test_validation_incorrect_file_extensions(self) -> None:
+        file_mock = FileMock(name="test.jpg")
+        with pytest.raises(ValidationError):
+            self.validation(value=file_mock)
+
+
+class TestValidateFileSize:
+    validation = ValidateFileSize(max_size=5_000_000)
+
+    def test_validation_file_size(self) -> None:
+        file_mock = FileMock(size=4_050_000)
+        result = self.validation(value=file_mock)
+        assert result is None
+
+    def test_validation_oversize(self) -> None:
+        file_mock = FileMock(size=5_050_000)
+        with pytest.raises(ValidationError):
+            self.validation(value=file_mock)
