3fd41b93d4fcc512a8daf866797142d5f4313ea0
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-10-16 17:27:21 +0300

    added class TestValidationAge

--- src/microblogging/core/tests/test_unit/test_validators/test_validators_form.py
+++ src/microblogging/core/tests/test_unit/test_validators/test_validators_form.py
@@ -6,35 +6,31 @@ from core.presentation.validators import ValidationAge
 from django.core.exceptions import ValidationError
 
 
-def test_validation_age_successfully() -> None:
+class TestValidationAge:
     validation = ValidationAge(min_age=18)
-    age_for_validation = datetime(2000, 1, 1)
-    result = validation(value=age_for_validation)
-    assert result is None
 
+    def test_validation_age_successfully(self) -> None:
+        age_for_validation = datetime(2000, 1, 1)
+        result = self.validation(value=age_for_validation)
+        assert result is None
 
-def test_validation_of_minimum_age_years() -> None:
-    validation = ValidationAge(min_age=18)
-    age_for_validation = datetime.now()
-    with pytest.raises(ValidationError):
-        validation(value=age_for_validation)
+    def test_validation_of_minimum_age_years(self) -> None:
+        age_for_validation = datetime.now()
+        with pytest.raises(ValidationError):
+            self.validation(value=age_for_validation)
 
+    def test_validation_of_minimum_age_month(self) -> None:
+        date = datetime.now()
+        age_for_validation = datetime(
+            year=date.year - 18, month=date.month + 1, day=date.day
+        )
+        with pytest.raises(ValidationError):
+            self.validation(value=age_for_validation)
 
-def test_validation_of_minimum_age_month() -> None:
-    validation = ValidationAge(min_age=18)
-    date = datetime.now()
-    age_for_validation = datetime(
-        year=date.year - 18, month=date.month + 1, day=date.day
-    )
-    with pytest.raises(ValidationError):
-        validation(value=age_for_validation)
-
-
-def test_validation_of_minimum_age_day() -> None:
-    validation = ValidationAge(min_age=18)
-    date = datetime.now()
-    age_for_validation = datetime(
-        year=date.year - 18, month=date.month, day=date.day + 1
-    )
-    with pytest.raises(ValidationError):
-        validation(value=age_for_validation)
+    def test_validation_of_minimum_age_day(self) -> None:
+        date = datetime.now()
+        age_for_validation = datetime(
+            year=date.year - 18, month=date.month, day=date.day + 1
+        )
+        with pytest.raises(ValidationError):
+            self.validation(value=age_for_validation)
