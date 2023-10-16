65f57355522802f6f327182dafea04d4a7b51f13
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-10-16 17:24:05 +0300

    added validation ValidationAge

--- src/microblogging/core/tests/test_unit/test_validators/test_validators_form.py
+++ src/microblogging/core/tests/test_unit/test_validators/test_validators_form.py
@@ -1,3 +1,4 @@
+# mypy: ignore-errors
 from datetime import datetime
 
 import pytest
@@ -8,7 +9,7 @@ from django.core.exceptions import ValidationError
 def test_validation_age_successfully() -> None:
     validation = ValidationAge(min_age=18)
     age_for_validation = datetime(2000, 1, 1)
-    result = validation(value=age_for_validation)  # type: ignore
+    result = validation(value=age_for_validation)
     assert result is None
 
 
