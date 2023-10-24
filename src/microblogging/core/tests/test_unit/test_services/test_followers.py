40e9846bbbc861a27b28afdf5783d9a0e2d50d2f
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-10-24 16:19:22 +0300

    added test for get_all_following func

--- src/microblogging/core/tests/test_unit/test_services/test_followers.py
+++ src/microblogging/core/tests/test_unit/test_services/test_followers.py
@@ -1,5 +1,5 @@
 import pytest
-from core.business_logic.services import get_all_followers
+from core.business_logic.services import get_all_followers, get_all_following
 from django.db.models.query import QuerySet
 
 
@@ -14,5 +14,20 @@ def test_get_all_followers_successfully() -> None:
 @pytest.mark.django_db
 def test_get_all_followers_non_existing_user() -> None:
     followers = get_all_followers(username="qwerty")
+    assert len(followers) == 0
+    assert list(followers) == []
+
+
+@pytest.mark.django_db
+def test_get_all_following_successfully() -> None:
+    followers = get_all_following(username="testuser1")
+    assert type(followers) == QuerySet
+    assert len(followers) == 2
+    assert followers[0]["following__username"] == "testuser2"
+
+
+@pytest.mark.django_db
+def test_get_all_following_non_existing_user() -> None:
+    followers = get_all_following(username="qwerty")
     assert len(followers) == 0
     assert list(followers) == []
