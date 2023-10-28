7c9d506a09b7529d8972433c0d832058f02de9c6
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-10-28 16:42:47 +0300

    added tests get_a_tag_to_search_for_tweets

--- src/microblogging/core/tests/test_unit/test_services/test_tags.py
+++ src/microblogging/core/tests/test_unit/test_services/test_tags.py
@@ -1,6 +1,7 @@
 import pytest
-from core.business_logic.services.tags import get_tags
+from core.business_logic.services.tags import get_a_tag_to_search_for_tweets, get_tags
 from core.models import Tags
+from django.db.models.query import QuerySet
 from django.db.utils import DataError
 
 
@@ -18,3 +19,21 @@ def test_get_tags_exceeding_max_len() -> None:
     tags = "t" * 21
     with pytest.raises(DataError):
         get_tags(tags=tags)
+
+
+@pytest.mark.django_db
+def test_get_a_tag_to_search_for_tweets_successfuly() -> None:
+    tag = "sql"
+    search_tag = get_a_tag_to_search_for_tweets(tag=tag)
+    assert type(search_tag) == QuerySet
+    assert len(search_tag) == 1
+    assert type(search_tag[0]) == Tags
+    assert search_tag[0].tag == tag
+
+
+@pytest.mark.django_db
+def test_get_a_tag_to_search_for_tweets_non_existent_tag() -> None:
+    tag = "test"
+    search_tag = get_a_tag_to_search_for_tweets(tag=tag)
+    assert type(search_tag) == QuerySet
+    assert len(search_tag) == 0
