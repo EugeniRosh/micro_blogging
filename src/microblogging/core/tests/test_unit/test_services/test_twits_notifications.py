fd07d74fbc04703c614ede53db1adfe0e1efbf14
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-01 14:39:45 +0300

    added test get_user_notifications

--- src/microblogging/core/tests/test_unit/test_services/test_twits_notifications.py
+++ src/microblogging/core/tests/test_unit/test_services/test_twits_notifications.py
@@ -1,5 +1,8 @@
 import pytest
-from core.business_logic.services.twits_notifications import adding_a_notification
+from core.business_logic.services.twits_notifications import (
+    adding_a_notification,
+    get_user_notifications,
+)
 from core.models import Profiles, Twits
 
 
@@ -10,3 +13,11 @@ def test_adding_a_notification_successfully() -> None:
     adding_a_notification(twit=twit, profile=profile)
     notifications = profile.notification.all()
     assert twit in notifications
+
+
+@pytest.mark.django_db
+def test_get_user_notifications_successfully() -> None:
+    profile = Profiles.objects.get(username="testuser1")
+    result = get_user_notifications(profile=profile)
+    assert len(result) == 2
+    assert result[0].created_at > result[1].created_at
