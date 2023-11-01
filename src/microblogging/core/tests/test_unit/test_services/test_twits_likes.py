754421acc64b6bd0ea3d171c0369653873e707a6
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-01 14:10:48 +0300

    changed message

--- src/microblogging/core/tests/test_unit/test_services/test_twits_likes.py
+++ src/microblogging/core/tests/test_unit/test_services/test_twits_likes.py
@@ -8,7 +8,7 @@ def test_like_a_twit_successfully() -> None:
     twit = Twits.objects.get(text="test text twit_2")
     profile = Profiles.objects.get(username="testuser3")
     result = like_a_twit(twit_id=twit.pk, profile=profile)
-    assert result == "like added"
+    assert result == "Like added"
     likes = twit.like.all()
     assert profile in likes
 
@@ -18,7 +18,7 @@ def test_like_a_twit_like_my_twit() -> None:
     twit = Twits.objects.get(text="test text twit_2")
     profile = Profiles.objects.get(username="testuser1")
     result = like_a_twit(twit_id=twit.pk, profile=profile)
-    assert result == "you can't like your tweet"
+    assert result == "You can't like your twit"
     likes = twit.like.all()
     assert profile not in likes
 
@@ -28,14 +28,14 @@ def test_deleting_a_twit_likes_successfully() -> None:
     twit = Twits.objects.get(text="test text twit_1")
     profile = Profiles.objects.get(username="testuser2")
     result = deleting_a_twit_likes(twit_id=twit.pk, profile=profile)
-    assert result == "like removed"
+    assert result == "Like removed"
     likes = twit.like.all()
     assert profile not in likes
 
 
 @pytest.mark.django_db
-def test_deleting_a_twit_likes_remove_like_my_tweet() -> None:
+def test_deleting_a_twit_likes_remove_like_my_twit() -> None:
     twit = Twits.objects.get(text="test text twit_1")
     profile = Profiles.objects.get(username="testuser1")
     result = deleting_a_twit_likes(twit_id=twit.pk, profile=profile)
-    assert result == "You can't delete likes on my tweet"
+    assert result == "You can't delete likes on my twit"
