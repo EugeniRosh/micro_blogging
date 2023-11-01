fe183a6baf35b4e56e7fd6a3bf3d552dd2f57b0e
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-01 13:49:11 +0300

    added test deleting_a_twit_likes

--- src/microblogging/core/tests/test_unit/test_services/test_twits_likes.py
+++ src/microblogging/core/tests/test_unit/test_services/test_twits_likes.py
@@ -1,5 +1,5 @@
 import pytest
-from core.business_logic.services import like_a_twit
+from core.business_logic.services import deleting_a_twit_likes, like_a_twit
 from core.models import Profiles, Twits
 
 
@@ -14,10 +14,28 @@ def test_like_a_twit_successfully() -> None:
 
 
 @pytest.mark.django_db
-def test_like_a_twit_like_my_tweet() -> None:
+def test_like_a_twit_like_my_twit() -> None:
     twit = Twits.objects.get(text="test text twit_2")
     profile = Profiles.objects.get(username="testuser1")
     result = like_a_twit(twit_id=twit.pk, profile=profile)
     assert result == "you can't like your tweet"
     likes = twit.like.all()
     assert profile not in likes
+
+
+@pytest.mark.django_db
+def test_deleting_a_twit_likes_successfully() -> None:
+    twit = Twits.objects.get(text="test text twit_1")
+    profile = Profiles.objects.get(username="testuser2")
+    result = deleting_a_twit_likes(twit_id=twit.pk, profile=profile)
+    assert result == "like removed"
+    likes = twit.like.all()
+    assert profile not in likes
+
+
+@pytest.mark.django_db
+def test_deleting_a_twit_likes_remove_like_my_tweet() -> None:
+    twit = Twits.objects.get(text="test text twit_1")
+    profile = Profiles.objects.get(username="testuser1")
+    result = deleting_a_twit_likes(twit_id=twit.pk, profile=profile)
+    assert result == "You can't delete likes on my tweet"
