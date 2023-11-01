464b38e647f23f28fe76c26920d88409352f90a0
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-01 15:07:47 +0300

    added test delete_repost_twits

--- src/microblogging/core/tests/test_unit/test_services/test_twits_repost.py
+++ src/microblogging/core/tests/test_unit/test_services/test_twits_repost.py
@@ -1,5 +1,5 @@
 import pytest
-from core.business_logic.services import repost_the_twit
+from core.business_logic.services import delete_repost_twits, repost_the_twit
 from core.models import Profiles, Twits
 
 
@@ -7,6 +7,7 @@ from core.models import Profiles, Twits
 def test_repost_the_twit_successfully() -> None:
     twit = Twits.objects.get(text="test text twit_3")
     profile = Profiles.objects.get(username="testuser2")
+    assert profile not in twit.repost.all()
     result = repost_the_twit(twit_id=twit.pk, profile=profile)
     assert result == "Repost added"
     reposts = twit.repost.all()
@@ -21,3 +22,22 @@ def test_repost_the_twit_repost_my_twit() -> None:
     assert result == "You can't repost your twit"
     reposts = twit.repost.all()
     assert profile not in reposts
+
+
+@pytest.mark.django_db
+def test_delete_repost_twits_reposts_successfully() -> None:
+    twit = Twits.objects.get(text="test text twit_1")
+    profile = Profiles.objects.get(username="testuser2")
+    assert profile in twit.repost.all()
+    result = delete_repost_twits(twit_id=twit.pk, profile=profile)
+    assert result == "Repost removed"
+    reposts = twit.repost.all()
+    assert profile not in reposts
+
+
+@pytest.mark.django_db
+def test_delete_repost_twits_remove_repost_my_twit() -> None:
+    twit = Twits.objects.get(text="test text twit_1")
+    profile = Profiles.objects.get(username="testuser1")
+    result = delete_repost_twits(twit_id=twit.pk, profile=profile)
+    assert result == "You can't delete repost on my twit"
