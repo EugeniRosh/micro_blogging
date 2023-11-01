e503ad08196b51581e9060fd753acd2454c24a0c
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-01 14:52:43 +0300

    added return str

--- src/microblogging/core/business_logic/services/twits_repost.py
+++ src/microblogging/core/business_logic/services/twits_repost.py
@@ -8,21 +8,21 @@ if TYPE_CHECKING:
     from core.models import Profiles, Twits
 
 
-def repost_the_twit(twit_id: int, profile: Profiles) -> None:
+def repost_the_twit(twit_id: int, profile: Profiles) -> str:
     twit: Twits = get_twit_by_id(twit_id=twit_id)
 
     if twit.profile == profile:
-        return None
+        return "You can't repost your twit"
 
     twit.repost.add(profile)
-    return None
+    return "Repost added"
 
 
-def delete_repost_twits(twit_id: int, profile: Profiles) -> None:
+def delete_repost_twits(twit_id: int, profile: Profiles) -> str:
     twit: Twits = get_twit_by_id(twit_id=twit_id)
 
     if twit.profile == profile:
-        return None
+        return "You can't delete repost on my twit"
 
     twit.repost.remove(profile)
-    return None
+    return "Repost removed"
