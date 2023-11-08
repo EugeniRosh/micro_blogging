c047edc6c7b95f32a356d61e2c909d5111934ea3
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-08 14:47:04 +0300

    added get_profile tests

--- src/microblogging/core/tests/test_unit/test_services/test_profile.py
+++ src/microblogging/core/tests/test_unit/test_services/test_profile.py
@@ -1,7 +1,7 @@
 import pytest
 from core.business_logic.exceptions import GetValueError
-from core.business_logic.services.profile import get_profile_by_username
-from core.models import Profiles
+from core.business_logic.services.profile import get_profile, get_profile_by_username
+from core.models import Profiles, Twits
 
 
 @pytest.mark.django_db
@@ -15,3 +15,20 @@ def test_get_profile_by_username_succssefully() -> None:
 def test_get_profile_by_username_raise_getvalueerror() -> None:
     with pytest.raises(GetValueError):
         get_profile_by_username(username="does_not_exist")
+
+
+@pytest.mark.django_db
+def test_get_profile_succssefully() -> None:
+    profile = Profiles.objects.get(username="testuser1")
+    test_profile, test_twits, test_follow = get_profile(username=profile.username)
+    assert test_profile == profile
+    assert type(test_follow) == bool
+    assert test_follow is False
+    assert type(test_twits) == list
+    assert type(test_twits[0]) == Twits
+
+
+@pytest.mark.django_db
+def test_get_profile_raise_getvalueerror() -> None:
+    with pytest.raises(GetValueError):
+        get_profile(username="does_not_exist")
