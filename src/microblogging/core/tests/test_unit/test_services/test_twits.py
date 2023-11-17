2dbc6db378d50163a09a8e96843df195c937b083
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-11-17 16:37:15 +0300

    added test get_twit_by_id

--- src/microblogging/core/tests/test_unit/test_services/test_twits.py
+++ src/microblogging/core/tests/test_unit/test_services/test_twits.py
@@ -1,5 +1,7 @@
 import pytest
+from core.business_logic.exceptions import GetValueError
 from core.business_logic.services.twits import (
+    get_twit_by_id,
     get_twits_and_counts_of_likes_of_reposts_of_answer,
 )
 from core.models import Twits
@@ -15,3 +17,20 @@ def test_get_twits_and_counts_of_likes_of_reposts_of_answer_succssefully() -> No
     assert type(twit.count_like) == int
     assert type(twit.count_repost) == int
     assert type(twit.count_answer) == int
+
+
+@pytest.mark.django_db
+def test_get_twit_by_id_succssefully() -> None:
+    twits = Twits.objects.all()
+    twit = twits[0]
+    twit_test = get_twit_by_id(twit_id=twit.pk)
+    assert type(twit_test) == Twits
+    assert twit == twit_test
+
+
+@pytest.mark.django_db
+def test_get_twit_by_id_raise_getvalueerror() -> None:
+    twits = Twits.objects.all()
+    twit_id_for_test = list(twits)[-1].pk + 1
+    with pytest.raises(GetValueError):
+        get_twit_by_id(twit_id=twit_id_for_test)
